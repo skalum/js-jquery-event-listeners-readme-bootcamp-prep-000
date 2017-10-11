0e1895c07c12102cd9099adefd5af8460ee6dffa
Sam Kalum <stkalum@gmail.com>
2017-10-11 20:40:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,19 +7,19 @@ function getIt() {
 function frameIt() {
   $("img").on("load", function() {
     $("img").addClass("tasty");
-  })
+  });
 }
 
 function pressIt() {
   $("input").on("keydown", function(key) {
     if(parseInt(key.which) === 71) alert("Pressed G!");
-  })
+  });
 }
 
 function submitIt() {
   $("form").on("submit", function () {
     alert("Your form is going to be submitted now.");
-  })
+  });
 }
 
 $(document).ready(function(){
